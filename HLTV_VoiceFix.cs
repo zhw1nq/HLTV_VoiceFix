1d48c1ef9cb6ba90ad9f734698c9b494fe6d1730
zhw1nq <132770113+zhw1nq@users.noreply.github.com>
2025-09-14 13:36:43 +0700

    Add files via upload

---
+++ HLTV_VoiceFix.cs
@@ -0,0 +1,32 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+
+namespace HLTV_VoiceFix;
+public class HLTV_VoiceFix : BasePlugin
+{
+    public override string ModuleName => "Fix Voice Chat in Demo Recordings";
+    public override string ModuleAuthor => "zhw1nq";
+    public override string ModuleVersion => "1.2.0";
+
+    public override void Load(bool hotReload)
+    {
+        RegisterEventHandler<EventRoundStart>(((roundStartEvent, eventInfo) =>
+        {
+            Server.NextFrame(() =>
+            {
+                HLTV_VoiceChat();
+            });
+            return HookResult.Continue;
+        }));
+    }
+
+    void HLTV_VoiceChat()
+    {
+        var hltvPlayer = Utilities.GetPlayers().Where(player => player.IsHLTV).FirstOrDefault();
+
+        if (hltvPlayer != null && hltvPlayer.IsValid)
+        {
+            hltvPlayer.VoiceFlags = VoiceFlags.All | VoiceFlags.ListenAll;
+        }
+    }
+}
